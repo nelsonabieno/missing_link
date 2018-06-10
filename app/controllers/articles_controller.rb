89cf7d1700c024bd6114469772dc8cce4511ccb6
nelsonabieno <=>
2018-06-10 20:08:57 +0100

    generate routes, controller and view for create article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+
+  # def index
+  #   @articles = Article.all
+  # end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
+end
