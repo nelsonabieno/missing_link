cbb0e700449ae1d53c85ee11f2fdc2b9ee265963
nelsonabieno <=>
2018-06-10 22:45:50 +0100

    create index,new and show actions and views for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,11 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
   private
 
   def article_params
