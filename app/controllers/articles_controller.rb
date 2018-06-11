1c62004ab0e780b8d9882417dd07456346035338
nelsonabieno <=>
2018-06-12 00:09:27 +0100

    implement edit and update actions with views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -11,22 +12,49 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if  @article.save
+    if @article.save
       flash[:notice] = 'article successfully saved!'
-      redirect_to articles_path
+      redirect_to article_path (@article)
     else
       render 'new'
     end
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+
+    if @article.update (article_params)
+      flash[:notice] = "Update successful"
+      redirect_to article_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+    if @article.destroy
+      flash[:notice] = "article successfully deleted"
+      redirect_to articles_path
+    else
+      render 'index'
+    end
+  end
 
   private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title,:description)
   end
 end
+
+
